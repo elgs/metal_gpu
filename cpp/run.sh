4d9918d6402cc58a73bfbd4d911d2ccc2d5ac5cd
Qian Chen <elgs1980@hotmail.com>
2023-06-21 03:56:39 -0700

    Update.

--- cpp/run.sh
+++ cpp/run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-clang++ -O3 -std=c++20 -fno-objc-arc -I ./metal-cpp main.cpp -framework Foundation -framework Metal -framework MetalKit
-NSZombieEnabled=YES OBJC_DEBUG_MISSING_POOLS=YES 
-./a.out
+clang++ -O3 -std=c++2b -fno-objc-arc -I ./metal-cpp main.cpp -framework Foundation -framework Metal -framework MetalKit
+NSZombieEnabled=NO OBJC_DEBUG_MISSING_POOLS=NO ./a.out
